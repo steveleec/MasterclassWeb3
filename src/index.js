d13a5f8c988999fc71d423277e4b358c031cad53
Lee Marreros <lee.marreros@pucp.pe>
2022-05-23 15:32:56 -0500

    updates with wiracocha and tatacuy

--- src/index.js
+++ src/index.js
@@ -1,21 +1,84 @@
-import { BigNumber, Contract, providers } from "ethers";
+import { BigNumber, Contract, providers, ethers } from "ethers";
+
+window.ethers = ethers;
 
 import {
+  initMarketplace,
+  listNftsOfAccount,
+  getListOfNftsForSale,
+} from "pachacuy-marketplace";
+
+import {
+  getContract,
+  getAllGameInformation,
+  getInformationByRank,
+} from "pachacuy-info";
+
+import {
+  init,
   approveBusd,
   allowance,
-  init,
+  // purchase
   purchaseGuineaPigWithBusd,
+  purchaseGuineaPigWithPcuy,
   purchaseLandWithBusd,
-  getListOfNftsPerAccount,
+  purchaseLandWithPcuy,
+  purchasePachaPassWithPcuy,
+  purchasePachaPassWithBusd,
+  // get NFT data
   getGuineaPigData,
   getLandData,
+  getPachaPassData,
+  getWalletData,
+  // helpers
+  isGuineaPigAllowedInPacha,
+  getListOfNftsPerAccount,
   balanceOf,
   totalSupply,
   tokenURI,
-  getWalletData,
+  signTatacuyTransaction,
+  mintTatacuy,
+  mintWiracocha,
+  // tatacuy
+  signTatacuyTxAndVerify,
+  finishTatacuyCampaign,
+  startTatacuyCampaign,
+  // wiracocha
+  signWiracochaTxAndReceivePcuy,
 } from "pachacuy-sc";
 
 var provider, account, signer;
+
+const domain = {
+  name: "My App",
+  version: "1",
+  chainId: 97,
+  verifyingContract: "0x1111111111111111111111111111111111111111",
+};
+
+const types = {
+  Mail: [
+    { name: "from", type: "Person" },
+    { name: "to", type: "Person" },
+    { name: "content", type: "string" },
+  ],
+  Person: [
+    { name: "name", type: "string" },
+    { name: "wallet", type: "address" },
+  ],
+};
+
+const mail = {
+  from: {
+    name: "Alice",
+    wallet: "0x2111111111111111111111111111111111111111",
+  },
+  to: {
+    name: "Bob",
+    wallet: "0x3111111111111111111111111111111111111111",
+  },
+  content: "Hello!",
+};
 
 function setUpListeners() {
   var bttn = document.getElementById("connect");
@@ -28,6 +91,7 @@ function setUpListeners() {
 
       provider = new providers.Web3Provider(window.ethereum);
       signer = provider.getSigner(account);
+      window.signer = signer;
     }
   });
 
@@ -36,6 +100,9 @@ function setUpListeners() {
     var wallet = document.getElementById("allowanceAmount").value;
     var _allowance = await allowance(wallet);
     console.log(_allowance.toString());
+
+    var res = await signTatacuyTransaction(signer, "10", "lee");
+    console.log(res);
   });
 
   var bttn = document.getElementById("approveButton");
@@ -101,12 +168,146 @@ function setUpListeners() {
     var balance = await balanceOf(value1, value2);
     console.log(balance.toString());
   });
+
+  var bttn = document.getElementById("ListOfNftsButton");
+  bttn.addEventListener("click", async function () {
+    var value = document.getElementById("listOfNfts").value;
+    var res = await listNftsOfAccount(value);
+    console.log(res);
+  });
+
+  var bttn = document.getElementById("ListOfNftsForSale");
+  bttn.addEventListener("click", async function () {
+    var res = await getListOfNftsForSale();
+    console.log(res);
+  });
+
+  var bttn = document.getElementById("MintTatacuy");
+  bttn.addEventListener("click", async function () {
+    var value = document.getElementById("mintTatacuyId").value;
+    var res = await mintTatacuy(signer, value);
+    console.log(res);
+  });
+
+  var bttn = document.getElementById("SignTatacuy");
+  bttn.addEventListener("click", async function () {
+    var guineaPigUuid = document.getElementById("signTatacuy0").value;
+    var likelihood = document.getElementById("signTatacuy1").value;
+    var pachaOwner = document.getElementById("signTatacuy2").value;
+    var pachaUuid = document.getElementById("signTatacuy3").value;
+    var res = await signTatacuyTxAndVerify(
+      signer,
+      guineaPigUuid,
+      likelihood,
+      pachaOwner,
+      pachaUuid
+    );
+    console.log(res);
+  });
+
+  var bttn = document.getElementById("SignWiracocha");
+  bttn.addEventListener("click", async function () {
+    var guineaPigUuid = document.getElementById("signWiracocha0").value;
+    var samiPoints = document.getElementById("signWiracocha1").value;
+    var pachaOwner = document.getElementById("signWiracocha2").value;
+    var pachaUuid = document.getElementById("signWiracocha3").value;
+    var res = await signWiracochaTxAndReceivePcuy(
+      signer,
+      guineaPigUuid,
+      samiPoints,
+      pachaOwner,
+      pachaUuid
+    );
+    console.log(res);
+  });
+
+  var bttn = document.getElementById("StartTatacuyC");
+  bttn.addEventListener("click", async function () {
+    var pachaUuid = document.getElementById("startttc1").value;
+    var tatacuyUuid = document.getElementById("startttc2").value;
+    var totalFunds = document.getElementById("startttc3").value;
+    var prizePerWinner = document.getElementById("startttc4").value;
+    var res = await startTatacuyCampaign(
+      signer,
+      pachaUuid,
+      tatacuyUuid,
+      totalFunds,
+      prizePerWinner
+    );
+    console.log(res);
+  });
+
+  var bttn = document.getElementById("FinishTatacuyC");
+  bttn.addEventListener("click", async function () {
+    var pachaUuid = document.getElementById("finishttc").value;
+    var { tatacuyOwner, totalSamiPoints, samiPointsClaimed, changeSamiPoints } =
+      await finishTatacuyCampaign(signer, pachaUuid);
+    console.log(tatacuyOwner);
+    console.log(totalSamiPoints.toString());
+    console.log(samiPointsClaimed.toString());
+    console.log(changeSamiPoints.toString());
+  });
 }
 
 async function initSmartContracts() {
-  // _account, price, _guineaPigId, _uuid, _raceAndGender
-  // _account, price, _ix, custodianWallet
-  var [NFT, PurhcaseContract] = await init(window.ethereum);
+  /**
+    * return [
+        nftpContract,
+        pacContract,
+        tataCuyContract,
+        wiracochaContract,
+        chakraContract,
+        hatunWasiContract,
+      ];
+   */
+  var [NFT, PurhcaseContract, TatacuyContract, WiracochaContract] = init(
+    window.ethereum
+  );
+  console.log(WiracochaContract);
+  WiracochaContract.on(
+    "WiracochaExchange",
+    (
+      exchanger,
+      pachaOwner,
+      pachaUuid,
+      amountPcuy,
+      totalPcuyBalance,
+      samiPoints,
+      ratePcuyToSami
+    ) => {
+      console.log("exchanger", exchanger);
+      console.log("pachaOwner", pachaOwner);
+      console.log("pachaUuid", pachaUuid.toString());
+      console.log("amountPcuy", amountPcuy.toString());
+      console.log("totalPcuyBalance", totalPcuyBalance.toString());
+      console.log("samiPoints", samiPoints.toString());
+      console.log("ratePcuyToSami", ratePcuyToSami.toString());
+    }
+  );
+
+  TatacuyContract.on(
+    "TatacuyTryMyLuckResult",
+    (
+      account,
+      hasWon,
+      prizeWinner,
+      likelihood,
+      pachaUuid,
+      tatacuyUuid,
+      pachaOwner,
+      idFromFront
+    ) => {
+      console.log("account", account);
+      console.log("hasWon", hasWon);
+      console.log("prizeWinner", prizeWinner);
+      console.log("likelihood", likelihood);
+      console.log("pachaUuid", pachaUuid);
+      console.log("tatacuyUuid", tatacuyUuid);
+      console.log("pachaOwner", pachaOwner);
+      console.log("idFromFront", idFromFront);
+    }
+  );
+
   PurhcaseContract.on(
     "GuineaPigPurchaseFinish",
     (_account, price, _guineaPigId, _uuid, _raceAndGender) => {
@@ -144,9 +345,9 @@ async function initSmartContracts() {
 }
 
 async function setUp() {
+  initSmartContracts();
   // init pachacuy-sc library
-  setUpListeners();
-  await initSmartContracts();
+  await setUpListeners();
 }
 
 setUp()
